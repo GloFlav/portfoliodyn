a6a0d833d67b177960f983bfbbda63c7fea59e2f
GloFlav <92213272+GloFlav@users.noreply.github.com>
2024-06-03 13:18:13 +0200

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -12,19 +12,19 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/PortfolioCli.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/PortfolioCli.png"
           title="PortFolio Command Line Interface"
           description="A Portfolio CLI form to know about the autor and imitate a command line in our exploitation system."
           link="https://gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/PortfolioDyn.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/PortfolioDyn.png"
           title="Interactive Website Portfolio"
           description="A dynamic portfolio."
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/feather.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/feather.png"
           title="Task Scheduling"
           description="Litle program to order and schedule task with their weight (duration or difficulty). A Web application demonstrating the best scheduling of tasks using the table and graph method."
           link="gloriaflavien.netlify.app"
@@ -32,19 +32,19 @@ const Projects = () => {
       </div>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/PortfolioCli.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/PortfolioCli.png"
           title="ONN Float board"
           description="A web application, focused on GIS, to manage the movements of automobile fleets at ONN (National Office of Nutrition) in Anosy, Antananarivo."
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/feather.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/feather.png"
           title="ENI Time Table schedule"
           description="A web application to automate the creation of the ENI timetable, considering the availability of teachers and the capacity of classrooms."
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/feather.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/feather.png"
           title="ONN Stock board"
           description="A web application for the logistics management of ONN."
           link="gloriaflavien.netlify.app"
