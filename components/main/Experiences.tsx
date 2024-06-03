a41055aa5f9ca8d201b24623b95f966cc1ce95ca
GloFlav <92213272+GloFlav@users.noreply.github.com>
2024-06-03 13:15:39 +0200

    Update Experiences.tsx

--- components/main/Experiences.tsx
+++ components/main/Experiences.tsx
@@ -18,25 +18,25 @@ const Experiences = () => {
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/idea.jpg"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/idea.jpg"
           title="2024"
           link="gloriaflavien.netlify.app"
           description="IDEA Academy Embassador"
         />
         <ProjectCard
-          src="/hotesse.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/hotesse.png"
           title="2024"
           description="Founder of ENI Hôtesse "
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/Eni.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/Eni.png"
           title="2024"
           description=" Founder and Organizer of ENI Novice Immersion (Tech Event)"
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/40eme.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/40eme.png"
           title="2024"
           description=" Organizer of the closing ceremony of the 40th anniversary of ENI"
           link="gloriaflavien.netlify.app"
@@ -44,25 +44,25 @@ const Experiences = () => {
       </div>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/AEENI.jpg"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/AEENI.jpg"
           title="2023"
           description="President of ENI Students Association"
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/MirayTournament.jpg"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/MirayTournament.jpg"
           title="2023"
           description=" Founder and Organizer of Miray Tournament (sport event)"
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/hackIT.jpg"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/hackIT.jpg"
           title="2023"
           description=" Cofounder and CO-Organizer of Hack-It Hackathon"
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/BachelorIT.jpg"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/BachelorIT.jpg"
           title="2023"
           description=" Founder and Organizer of Bachelor IT (Tech Event and Forum )"
           link="gloriaflavien.netlify.app"
@@ -70,25 +70,25 @@ const Experiences = () => {
       </div>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/feather.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/feather.png"
           title="2022"
           description="Designer and developper at ONN (Intern)"
           link="gloriaflavien.netlify.app"
         />
         <ProjectCard
-          src="/feather.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/feather.png"
           title="2021"
           link="gloriaflavien.netlify.app"
           description="Designer and developper at CIDST (Intern) "
         />
         <ProjectCard
-          src="/feather.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/feather.png"
           title="2021"
           link="https://www.gloriaflavien.netlify.app"
           description=" Designer and developper at Maison de la Communication Universitaire (Intern)"
         />
         <ProjectCard
-          src="/feather.png"
+          src="https://raw.githubusercontent.com/GloFlav/portfoliodyn/main/public/feather.png"
           title="2021"
           description=" Organizer of 'Salon de l'étudiant et de l'emploi 3rd edition'"
           link="gloriaflavien.netlify.app"
